8aaa95563d2aea410c0a2203d32eb581c0be1bab
matixoni <matixoni@o2.pl>
2017-11-09 19:44:33 +0100

    changing in project structure and add some frameworks for app

---
+++ MedicalSrv/Medical/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from .common.choices import *
+from django.contrib.auth.models import User
+
+
+class Institute(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=30)
+
+
+class UserProfile(models.Model):
+    Id = models.AutoField(primary_key=True)
+    User = models.OneToOneField(User)
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Title = models.CharField(max_length=30)
+    Institute = models.ForeignKey(Institute, on_delete=None)
+
+
+class Patient(models.Model):
+    Id = models.AutoField(primary_key=True)
+    PIN = models.IntegerField()
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Sex = models.IntegerField(choices=Sex, default=1)
+    BirthDate = models.DateTimeField()
+    CatalogPath = models.CharField(max_length=30)
+    CreationDate = models.DateTimeField()
+    Doctor = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+
+class Vist(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Date = models.DateTimeField()
+    CreteDate = models.DateTimeField()
+    Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+
+class Photography(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=30)
+    Directory = models.CharField(max_length=100)
+    Discription = models.CharField(max_length=1000)
+    Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+
+class Disease(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=300)
+    Photography = models.ForeignKey(Photography, on_delete=models.CASCADE)
+    Discription = models.CharField(max_length=1000)
+
+
+class Treatment(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    Descritpion = models.CharField(max_length=100)
+
+
+class Medicine(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    AdministerOfDosages = models.CharField(max_length=100)
+    Treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
---
+++ MedicalSrv/Medical/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import *
+from django.template import loader
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+
+def Log(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user, backend=None)
+                return HttpResponseRedirect('/main/')
+    return render(request, 'Medical/login.html')
+
+
+@login_required(login_url='/login/')
+def Index(request):
+    return render(request, 'Medical/index.html')
+
+
+@login_required(login_url='/login/')
+def OnRegistration(request):
+    return render(request, 'Medical/Registration/main-registration.html')
+
+
+@login_required(login_url='/login/')
+def OnPatients(request):
+    return render(request, 'Medical/Patients/main-patients.html')
+
+
+@login_required(login_url='/login/')
+def OnCalendar(request):
+    return render(request, 'Medical/Calendar/main-calendar.html')
